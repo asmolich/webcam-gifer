862816984898ba38b7a2b94a6957e6620f0fea31
asmolich <a.smolich@gmail.com>
2013-06-01 11:18:51 +0200

    app engine app structure

---
+++ hw.py
@@ -0,0 +1,18 @@
+from google.appengine.ext import webapp
+
+from google.appengine.ext.webapp import util
+
+
+class HwHandler(webapp.RequestHandler):
+    def get(self):
+        self.response.out.write('Hi there!')
+
+
+def main():
+    application = webapp.WSGIApplication([('/', HwHandler)],
+                                         debug=True)
+    util.run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
