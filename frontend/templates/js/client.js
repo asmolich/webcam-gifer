6aa81acb9c199f967930758f94ac24de03e4bfba
john <john@hesse>
2013-06-01 13:40:49 +0200

    video capture working

--- frontend/templates/js/client.js
+++ frontend/templates/js/client.js
@@ -2,7 +2,7 @@ $('#mainLinkAbout').click(function () {
     $('#canvasHome').addClass('hidden');
     $('#canvasNewCapture').addClass('hidden');
     $('#canvasAbout').removeClass('hidden');
-    $('#mainLinkMain').parent().removeClass('active'); 
+    $('#mainLinkMain').parent().removeClass('active');
     $('#mainLinkAbout').parent().addClass('active');
 
 
@@ -23,4 +23,50 @@ $('#btnStartNewCapture').click(function () {
     $('#canvasHome').addClass('hidden');
     $('#canvasNewCapture').removeClass('hidden');
 
+
+    var video = document.getElementById("live")
+
+    navigator.webkitGetUserMedia({video: true },
+        function (stream) {
+            video.src = window.webkitURL.createObjectURL(stream)
+        },
+        function (err) {
+            console.log("Unable to get video stream!")
+        }
+    )
+
 });
+
+// initilize video
+
+
+var snap = function () {
+    var live = document.getElementById("live");
+    var snapshot = document.getElementById("snapshot");
+    var filmroll = document.getElementById("filmroll");
+
+    // Make the canvas the same size as the live video
+    snapshot.width = live.clientWidth;
+    snapshot.height = live.clientHeight;
+
+    // Draw a frame of the live video onto the canvas
+    var c = snapshot.getContext("2d");
+    c.drawImage(live, 0, 0, snapshot.width, snapshot.height);
+
+    // Create an image element with the canvas image data
+    var img = document.createElement("img");
+    img.src = snapshot.toDataURL("image/png");
+    img.style.padding = 5;
+    img.width = snapshot.width / 2;
+    img.height = snapshot.height / 2;
+
+    // Add the new image to the film roll
+    filmroll.appendChild(img);
+
+
+};
+
+
+$('#btnTakePicture').click(snap);
+
+
